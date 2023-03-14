d39d8bf6362c411eb585cca5eb6cbeac499cc35e
Kris Hvattum <klhvattum@live.com>
2023-03-13 18:10:24 -0700

    Logo styling

--- src/components/FeaturedVideo.js
+++ src/components/FeaturedVideo.js
@@ -5,7 +5,7 @@ const FeaturedVideo = ({ video, poster }) => {
     <Container>
       <Row className='justify-content-center'>
         <Col xl='9'>
-                <video className='my-2 featuredVideo' src={video} width='100%' height='100%' autoPlay  playsInline muted loop poster={poster} controls></video>
+                <video className='my-2 featuredVideo' src={video} width='100%' height='100%' autoPlay  playsInline muted loop poster={poster}></video>
         </Col>
       </Row>
     </Container>   
